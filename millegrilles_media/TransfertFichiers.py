e1eb0a7540b5491daec4746351dae2d64fadda1e
mathieu <mathieu.dugre@mdugre.info>
2023-06-30 20:57:57 -0400

    Fix batch upload

--- millegrilles_media/TransfertFichiers.py
+++ millegrilles_media/TransfertFichiers.py
@@ -9,6 +9,52 @@ BATCH_UPLOAD_DEFAULT = 100_000_000
 CHIFFRER_CHUNK_SIZE = 64 * 1024
 
 
+class TransportStream(asyncio.ReadTransport):
+
+    def __init__(self):
+        super().__init__()
+        self.reading = True
+        self.closed = False
+
+    def close(self) -> None:
+        self.closed = True
+
+    def is_reading(self) -> bool:
+        return self.reading
+
+    def pause_reading(self) -> None:
+        self.reading = False
+
+    def resume_reading(self) -> None:
+        self.reading = True
+
+
+async def feed_filepart(tmp_file, stream, limit=BATCH_UPLOAD_DEFAULT):
+    taille_uploade = 0
+    done = False
+    transport = TransportStream()
+    stream.set_transport(transport)
+
+    while taille_uploade < limit:
+        while not transport.is_reading():
+            if transport.closed:
+                raise Exception('stream closed')
+            await asyncio.sleep(0.1)
+
+        chunk = tmp_file.read(CHIFFRER_CHUNK_SIZE)
+        stream.feed_data(chunk)
+        taille_uploade += len(chunk)
+
+        if not chunk:
+            done = True
+            break
+
+        await asyncio.sleep(0.001)  # Yield
+
+    stream.feed_eof()
+    return taille_uploade, done
+
+
 async def uploader_fichier(session: aiohttp.ClientSession, etat_media, fuuid,
                            tmp_file: tempfile.TemporaryFile,
                            batch_size=BATCH_UPLOAD_DEFAULT):
@@ -17,8 +63,26 @@ async def uploader_fichier(session: aiohttp.ClientSession, etat_media, fuuid,
 
     tmp_file.seek(0)
     headers = {'x-fuuid': fuuid}
-    async with session.put(f'{url_fichier}/0', ssl=ssl_context, headers=headers, data=tmp_file) as resp:
-        resp.raise_for_status()
+
+    done = False
+    position = 0
+    while not done:
+        # Creer stream-reader pour lire chunks d'un fichier
+        stream = asyncio.StreamReader()
+        session_coro = session.put(f'{url_fichier}/{position}', ssl=ssl_context, headers=headers, data=stream)
+        stream_coro = feed_filepart(tmp_file, stream, limit=batch_size)
+
+        # Uploader chunk
+        session_response = None
+        try:
+            session_response, stream_response = await asyncio.gather(session_coro, stream_coro)
+        finally:
+            if session_response is not None:
+                session_response.release()
+
+        # Incrementer position pour prochain chunk
+        position += stream_response[0]
+        done = stream_response[1]
 
     async with session.post(url_fichier, ssl=ssl_context, headers=headers) as resp:
         resp.raise_for_status()
