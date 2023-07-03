ecfda65757ddd7419a19ee35fc5203c575da1ff2
mathieu <mathieu.dugre@mdugre.info>
2023-07-03 13:39:23 -0400

    Fix version package dist

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def get_version():
     try:
         with open('version.txt', 'r') as version_file:
             version = version_file.readline().strip()
-    except FileNotFoundError as e:
+    except FileNotFoundError:
         with open('build.txt', "r") as buildno_file:
             build_no = buildno_file.read().strip()
 
