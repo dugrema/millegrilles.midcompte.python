35735306684a8555b53b299ba3d491ad6d997596
Mathieu Dugre <mathieu.dugre@mdugre.info>
2023-09-17 17:05:02 -0400

    Fix upload secondaire

--- millegrilles_fichiers/Synchronisation.py
+++ millegrilles_fichiers/Synchronisation.py
@@ -892,7 +892,6 @@ class SyncManager:
 
                 async def cb_upload():
                     nonlocal dernier_update
-                    nonlocal connection
                     now = datetime.datetime.utcnow()
                     if now - intervalle_update > dernier_update:
                         dernier_update = now
@@ -908,7 +907,7 @@ class SyncManager:
                 self.__upload_en_cours = etat_upload
                 pending = {
                     asyncio.create_task(uploader_fichier(session, self.__etat_instance, etat_upload)),
-                    asyncio.create_task(self.__run_emettre_etat_upload(fuuid, connection, producer, event_done)),
+                    asyncio.create_task(self.__run_emettre_etat_upload(fuuid, producer, event_done)),
                 }
                 done, pending = await asyncio.wait(pending, return_when=asyncio.FIRST_COMPLETED)
 
@@ -934,30 +933,35 @@ class SyncManager:
                         raise e
 
             # Transfert termine. Supprimer job d'upload
-            await asyncio.to_thread(connection.supprimer_job_upload, fuuid)
+            async with SQLiteWriteOperations(connection) as dao_write:
+                await asyncio.to_thread(dao_write.supprimer_job_upload, fuuid)
         except ClientResponseError as e:
             if e.status == 409:
                 self.__logger.info("upload_fichier_primaire Le fichier %s existe deja sur le serveur - OK, terminer job immediatement" % fuuid)
-                await asyncio.to_thread(connection.supprimer_job_upload, fuuid)
+                async with SQLiteWriteOperations(connection) as dao_write:
+                    await asyncio.to_thread(dao_write.supprimer_job_upload, fuuid)
         except FileNotFoundError as e:
             self.__logger.info(
                 "upload_fichier_primaire Le fichier %s n'existe pas localement : %s" % (fuuid, e))
-            await asyncio.to_thread(connection.supprimer_job_upload, fuuid)
+            async with SQLiteWriteOperations(connection) as dao_write:
+                await asyncio.to_thread(dao_write.supprimer_job_upload, fuuid)
         except Exception:
             self.__logger.exception('upload_fichier_primaire Erreur upload fichier vers primaire')
-            await asyncio.to_thread(connection.touch_upload, fuuid, -1)
+            async with SQLiteWriteOperations(connection) as dao_write:
+                await asyncio.to_thread(dao_write.touch_upload, fuuid, -1)
 
         self.__upload_en_cours = None
 
-    async def __run_emettre_etat_upload(self, fuuid: str, entretien_db, producer, event_stop: asyncio.Event):
-        while event_stop.is_set() is False:
-            try:
-                await self.emettre_etat_upload(fuuid, entretien_db, producer)
-                await asyncio.wait_for(event_stop.wait(), timeout=5)
-            except asyncio.TimeoutError:
-                pass  # OK
-            except asyncio.CancelledError:
-                return  # Stopped
+    async def __run_emettre_etat_upload(self, fuuid: str, producer, event_stop: asyncio.Event):
+        with self.__etat_instance.sqlite_connection() as connection:
+            while event_stop.is_set() is False:
+                try:
+                    await self.emettre_etat_upload(fuuid, connection, producer)
+                    await asyncio.wait_for(event_stop.wait(), timeout=5)
+                except asyncio.TimeoutError:
+                    pass  # OK
+                except asyncio.CancelledError:
+                    return  # Stopped
 
     async def emettre_etat_upload(self, fuuid, connection: SQLiteConnection, producer):
 
@@ -985,15 +989,18 @@ class SyncManager:
             else:
                 taux = None
 
-        await asyncio.to_thread(entretien_db.touch_upload, fuuid, None)
-        try:
-            etat = await asyncio.to_thread(entretien_db.get_etat_uploads)
-            nombre = etat['nombre']
-            taille = etat['taille']
-        except TypeError:
-            # Aucuns downloads
-            nombre = None
-            taille = None
+        async with SQLiteWriteOperations(connection) as dao_write:
+            await asyncio.to_thread(dao_write.touch_upload, fuuid, None)
+
+        async with SQLiteReadOperations(connection) as dao_read:
+            try:
+                etat = await asyncio.to_thread(dao_read.get_etat_uploads)
+                nombre = etat['nombre']
+                taille = etat['taille']
+            except TypeError:
+                # Aucuns downloads
+                nombre = None
+                taille = None
 
         self.__logger.debug("emettre_etat_upload %s fichiers, %s bytes transfere a %s KB/sec (courant: %s/%s)" % (nombre, taille, taux, position_en_cours, taille_en_cours))
 
