d0f945fc8871b300f82c053aa6429e3da527597f
Mathieu Dugre <mathieu.dugre@mdugre.info>
2024-01-12 14:16:39 -0500

    Fix position event resume download

--- millegrilles_fichiers/Synchronisation.py
+++ millegrilles_fichiers/Synchronisation.py
@@ -820,6 +820,7 @@ class SyncManager:
         else:
             # Tenter de resumer le download
             position = stat_fichier_work.st_size
+            self.__download_en_cours['position'] = position
             taille_fichier = self.__download_en_cours['taille']
             headers = {'Range': 'bytes=%s-%s/%s' % (position, taille_fichier-1, taille_fichier)}
             self.__logger.debug("Resumer %s a position %s" % (fuuid, headers))
