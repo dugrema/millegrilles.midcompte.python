480fecd2b07dc112208e901e5f8d8ba646b0b21a
Mathieu Dugre <mathieu.dugre@mdugre.info>
2024-09-21 11:23:01 -0400

    Update setup for jenkins build

--- setup.py
+++ setup.py
@@ -1,28 +1,12 @@
 from setuptools import setup, find_packages
-import subprocess
+from os import environ
 
-
-def get_version():
-    try:
-        with open('version.txt', 'r') as version_file:
-            version = version_file.readline().strip()
-    except FileNotFoundError:
-        with open('build.txt', "r") as buildno_file:
-            build_no = buildno_file.read().strip()
-
-        # commande_git_version = ['git', 'name-rev', '--name-only', 'HEAD']
-        commande_git_version = ['git', 'rev-parse', '--abbrev-ref', 'HEAD']
-        output_process = subprocess.run(commande_git_version, stdout=subprocess.PIPE)
-        version = output_process.stdout.decode('utf8').strip()
-        version = '%s.%s' % (version, build_no)
-        print("Version: %s" % (version))
-
-    return version
+__VERSION__ = environ.get("VBUILD") or "2024.0.0"
 
 
 setup(
     name='millegrilles_midcompte',
-    version='%s' % get_version(),
+    version=__VERSION__,
     packages=find_packages(),
     url='https://github.com/dugrema/millegrilles.midcompte.python',
     license='AFFERO',
@@ -33,6 +17,7 @@ setup(
         'pytz>=2020.4',
         'pymongo>=3.11.2,<4.0',
         'aiohttp>=3.8.1,<4',
-        'requests>=2.28.1,<3'
+        'requests>=2.28.1,<3',
+        'pyjwt'
     ]
 )
