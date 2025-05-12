4dc85ee1ec98ae512902a91040017c3d39aa13fc
Mathieu Dugre <mathieu.dugre@mdugre.info>
2025-05-12 10:47:11 -0400

    Added stages to media Docker build

--- millegrilles_media/setup.py
+++ millegrilles_media/setup.py
@@ -1,28 +1,12 @@
 from setuptools import setup, find_packages
-import subprocess
+from os import environ
 
-
-def get_version():
-    try:
-        with open('version.txt', 'r') as version_file:
-            version = version_file.readline().strip()
-    except FileNotFoundError as e:
-        with open('../build.txt', "r") as buildno_file:
-            build_no = buildno_file.read().strip()
-
-        # commande_git_version = ['git', 'name-rev', '--name-only', 'HEAD']
-        commande_git_version = ['git', 'rev-parse', '--abbrev-ref', 'HEAD']
-        output_process = subprocess.run(commande_git_version, stdout=subprocess.PIPE)
-        version = output_process.stdout.decode('utf8').strip()
-        version = '%s.%s' % (version, build_no)
-        print("Version: %s" % (version))
-
-    return version
+__VERSION__ = environ.get("VBUILD") or "2025.3.0"
 
 
 setup(
     name='millegrilles_media',
-    version='%s' % get_version(),
+    version=__VERSION__,
     packages=find_packages(),
     url='https://github.com/dugrema/millegrilles.midcompte.python',
     license='AFFERO',
